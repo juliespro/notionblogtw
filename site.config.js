3266fd5fced6997416c8aeb7106ceaabc89b04fb
txs <txshon@gmail.com>
2021-10-20 20:37:51 +0800

    make another repo for chinese specific blog domain

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '5389ec34f0344c9c8247df6bc1848222',
+  rootNotionPageId: '783310b435dd4d419c31a535dbadf818',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
